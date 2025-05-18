8100cb03bc4f9a5490c905f30f0180dd774b5ccf
waltermangwiro <walter.mangwiro19@gmail.com>
2025-05-18 21:01:02 +0200

    Initial commit

---
+++ src/main/java/pl/edu/vistula/first_java_project_spring/FirstJavaProjectSpringApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.vistula.first_java_project_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaProjectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaProjectSpringApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/edu/vistula/first_java_project_spring/FirstJavaProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_java_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
